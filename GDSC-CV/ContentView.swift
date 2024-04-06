ee9291999ede43e0fa4dc9a714ee637d6971a33b
chris901003 <a0987999103@gmail.com>
2024-04-06 16:24:57 +0800

    Initial Commit

---
+++ GDSC-CV/ContentView.swift
@@ -0,0 +1,25 @@
+// Created for GDSC-CV in 2024
+// Using Swift 5.0
+//
+//
+// Created by HongYan on 2024/4/6.
+// Copyright © 2024 HongYan. All rights reserved.
+
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GDSC-CV/GDSC_CVApp.swift
@@ -0,0 +1,18 @@
+// Created for GDSC-CV in 2024
+// Using Swift 5.0
+//
+//
+// Created by HongYan on 2024/4/6.
+// Copyright © 2024 HongYan. All rights reserved.
+
+
+import SwiftUI
+
+@main
+struct GDSC_CVApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
